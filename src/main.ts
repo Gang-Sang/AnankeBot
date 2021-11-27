23b130b42b221e81b3de48f868c1af33a9f7e28f
TheIberian <sangx001@gmail.com>
2021-11-26 20:23:38 -0600

    add buy transaction

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import Web3 from 'web3';
 import { Mutex } from 'async-mutex';
 import { log } from './common/logger';
 import config from './appConfig.json';
-import { getPlatformToExecute } from './main/platforms';
+import { Platform, getPlatformToExecute } from './main/platforms';
 import { getBalace } from './main/erc20';
-import { getLiquidityReserves } from './main/spookySwap';
+import { getLiquidityReserves, swapDaiForTokens } from './main/spookySwap';
 
 const mutex = new Mutex();
 let currentlyRunning = false;
@@ -19,7 +19,7 @@ export const mainProgramLoop = async () => {
 			continue;
 		}
 
-		mainFunction(web3);
+		await mainFunction(web3);
 		await sleep(10 * 1000);
 	}
 }
@@ -55,6 +55,8 @@ async function mainFunction(web3: Web3) {
 	const executePlatform = await getPlatformToExecute(web3, currentBlock);
 	if(!executePlatform) { return; }
 
+	log(`Starting buying cycle for ${executePlatform.name}`);
+
 	//start the buy process
 	const daiBalance = await getBalace(web3, config.daiTokenAddress);
 	if (daiBalance <= 0 ) {
@@ -62,19 +64,24 @@ async function mainFunction(web3: Web3) {
 		return;
 	}
 
-    //check rugpull metrics
-    const reserves = await getLiquidityReserves(web3, executePlatform);
-    if(reserves[1] < 1000000)
-    {
-        log(`Dai in LP balance too low, might be a rugpull. Dai Liquidity ${reserves[1]}`);
-        return;
-    }
+	//check rugpull metrics
+	const reserves = await getLiquidityReserves(web3, executePlatform);
+	if(reserves[1] < 1000000)
+	{
+		log(`Dai in LP balance too low, might be a rugpull. Dai Liquidity ${reserves[1]}`);
+		return;
+	}
 
 	//trade dai
+	if(await buyDai(web3, executePlatform, currentBlock, 100, reserves)) {
+		log('Dai buy transaction sucessful');
+	} else {
+		log('Dai buy transaction failed');
+		return;
+	}
 
-	
 	//stake token
-
+                                                                                               
 	//watch for rebase
 
 	//unstake token
@@ -84,4 +91,30 @@ async function mainFunction(web3: Web3) {
 	await SetCurrentlyRunning(false);
 }
 
+const buyDai = async (web3: Web3, platform: Platform, currentBlock: number, daiAmount: number, reserves: number[]) => {
+	const loopLimit = 120;
+	let loops = 0;
+	let reciept = await swapDaiForTokens(web3, platform, currentBlock, daiAmount, reserves);
 
+	if(config.verbose) {
+		log(`Dai buy transaction created - hash - ${reciept.transactionHash}`);
+	}
+
+	while (loops < loopLimit) {
+		await sleep(10 * 1000);
+		reciept = await web3.eth.getTransactionReceipt(reciept.transactionHash);
+
+		if(reciept?.blockNumber) {
+			if(reciept.status == true) {
+				return true;
+			} else {
+				
+				return false;
+			}
+		}
+		loops++;
+	}
+
+	log('Dai buy transaction status unknown. Exiting, manually check transaction');
+	throw 'Dai buy transaction status unknown';
+}
--- src/main/spookySwap.ts
+++ src/main/spookySwap.ts
@@ -6,33 +6,33 @@ import uniswapPairAbi from '../abi/unswapV2Pair.json';
 import spookyRouterAbi from '../abi/spookySwapRouter.json';
 
 export const getLiquidityReserves = async (web3: Web3, platform: Platform) => {
-    const myContract = new web3.eth.Contract(uniswapPairAbi as any, platform.daiLPPoolContract);
-    const reserves = await (myContract as any).methods.getReserves().call();
-    return [reserves._reserve0 / Math.pow(10, 9), reserves._reserve1 / Math.pow(10, 18)];
+	const myContract = new web3.eth.Contract(uniswapPairAbi as any, platform.daiLPPoolContract);
+	const reserves = await (myContract as any).methods.getReserves().call();
+	return [reserves._reserve0 / Math.pow(10, 9), reserves._reserve1 / Math.pow(10, 18)];
 }
 
 export const swapDaiForTokens = async (web3: Web3, platform: Platform, currentBlock: number, daiAmount: number, reserves: number[]) => {
-    const currentPrice = reserves[1] / reserves[0];
-    const minOut = ((daiAmount / currentPrice) * .999) * Math.pow(10, platform.numberOfDecimals);
-    const path = [config.daiTokenAddress, platform.tokenContract];
-    const deadline = currentBlock + (config.numOfBlockPreBuy / 2);
-    const routerContract = new web3.eth.Contract(spookyRouterAbi as any, config.spookRouterAddress);
-    
-    const callData = await routerContract.methods.swapExactTokensForTokens(daiAmount * Math.pow(10, 18), minOut, path, config.publicKey, deadline);
-    const gasEstimate = 1.2 * (await routerContract.methods.swapExactTokensForTokens(daiAmount * Math.pow(10, 18), minOut, path, config.publicKey, deadline).estimateGas());
-    const gasPrice = parseFloat(await web3.eth.getGasPrice()) * 1.2;
-    const transaction : TransactionConfig = {
-        from: config.publicKey,
-        to: config.spookRouterAddress,
-        data: callData,
-        value: '0',
-        gasPrice: gasPrice,
-        gas: gasEstimate
-    };
-    const signTx = await web3.eth.accounts.signTransaction(transaction, config.privateKey);
-    const rawTx: string = signTx.rawTransaction as string;
+	const currentPrice = reserves[1] / reserves[0];
+	const minOut = ((daiAmount / currentPrice) * .999) * Math.pow(10, platform.numberOfDecimals);
+	const path = [config.daiTokenAddress, platform.tokenContract];
+	const deadline = currentBlock + (config.numOfBlockPreBuy / 2);
+	const routerContract = new web3.eth.Contract(spookyRouterAbi as any, config.spookRouterAddress);
 
-    return await web3.eth.sendSignedTransaction(rawTx);
+	const callData = await routerContract.methods.swapExactTokensForTokens(daiAmount * Math.pow(10, 18), minOut, path, config.publicKey, deadline);
+	const gasEstimate = 1.2 * (await routerContract.methods.swapExactTokensForTokens(daiAmount * Math.pow(10, 18), minOut, path, config.publicKey, deadline).estimateGas());
+	const gasPrice = parseFloat(await web3.eth.getGasPrice()) * 1.2;
+	const transaction: TransactionConfig = {
+		from: config.publicKey,
+		to: config.spookRouterAddress,
+		data: callData,
+		value: '0',
+		gasPrice: gasPrice,
+		gas: gasEstimate
+	};
+	const signTx = await web3.eth.accounts.signTransaction(transaction, config.privateKey);
+	const rawTx: string = signTx.rawTransaction as string;
+
+	return await web3.eth.sendSignedTransaction(rawTx);
 }
 
 export const swapTokensForDai = (web3: Web3, platform: Platform) => {
